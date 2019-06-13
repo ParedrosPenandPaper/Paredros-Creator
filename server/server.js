e0292d391c0528401d59a073af8a5d4524af4bf9
agcharis67705 <agcharis67705@th-nuernberg.de>
2019-06-13 19:05:55 +0200

    serving static content with node

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '192.168.0.3';
 const port = 80;
 
+
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  fs.readFile('./dist/index.html', (error, fileContent) => {
+    if(error){
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('error - page could not be loaded!');
+    }
+    else{
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.write(fileContent);
+      res.end();
+    }
+  })
 });
 
 server.listen(port, hostname, () => {
