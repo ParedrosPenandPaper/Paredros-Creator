ff45ad12cc1d5eafa89a0c9d7447db77964dd0d9
agcharis67705 <ismael_agchar@gmx.de>
2019-06-18 18:10:04 +0200

    mongo hopefully working

--- server/server.js
+++ server/server.js
@@ -4,8 +4,30 @@ const express = require('express')
 const app = express()
 const port = 80;
 
+const MongoClient = require('mongodb').MongoClient
+const dbConn = 'mongodb://paredros-creator-db:27017/paredrosCreatorDb'
+
 app.use(express.static(__dirname + '/dist'))
 
+app.get('/mongo', (req, res) => {
+    MongoClient.connect(dbConn, {useNewUrlParser: true})
+    .then(db => {
+        db.collection('adventures').find({})
+        .then(result => {
+            db.close()
+            res.status(200).send(result)
+        })
+        .catch(err => {
+            db.close()
+            res.status(400).send(err)
+        })
+    })
+    .catch(err => {
+        db.close()
+        res.status(400).send(err)
+    })
+})
+
 app.listen(port, () => {
-    console.log('paredros creator running on port 80')
+    console.log('paredros creator running on port ' + port)
 })
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { store } from './store/store.js'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
+
+// new Vue({
+//   el: '#app',
+//   store,
+//   components: { App },
+//   template: '<App/>'
+// })
