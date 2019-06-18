5c6a61ce8152430ba60dd5142aa56c8a8f737820
agcharis67705 <ismael_agchar@gmx.de>
2019-06-18 16:02:22 +0200

    minor changes

--- server/server.js
+++ server/server.js
@@ -1,14 +1,11 @@
 'use strict'
 
 const express = require('express')
-// const hostname = '192.168.0.3'
-
+const app = express()
 const port = 80;
 
-const app = express()
 app.use(express.static('../dist'))
 
 app.listen(port, () => {
     console.log('paredros creator running on port 80')
-    console.log()
 })
