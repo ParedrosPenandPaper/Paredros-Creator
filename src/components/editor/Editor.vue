2ddc230e65568163b4b9035f17465eacdb86be68
agcharis67705 <agcharis67705@th-nuernberg.de>
2019-07-30 13:11:24 +0200

    repo cleanup for Semesterferien workpower

--- src/components/editor/Editor.vue
+++ src/components/editor/Editor.vue
@@ -1,43 +1,17 @@
 <template>
     <div class="editor-container">
-        <meta-panel
-                :meta="data.meta"
-        ></meta-panel>
-        <adventure-panel
-                :adventure="data.adventure"
-                @scene-paragraph-adventure-received="receiveSceneChapter"
-                @chapter-paragraph-adventure-received="receiveChapterParagraph"
-                @adventure-paragraph-received="receiveAdventureParagraph"
-                @delete-scene-adventure="deleteScene"
-        ></adventure-panel>
+
     </div>
 </template>
 
 <script>
-    import MetaPanel from './Meta_Panel.vue'
-    import AdventurePanel from './Adventure_Panel.vue'
-
     export default {
         props: {
             data: Object
         },
         components: {
-            MetaPanel,
-            AdventurePanel
         },
         methods: {
-            receiveSceneChapter(chapter){
-                this.$emit('scene-paragraph-edited', chapter)
-            },
-            receiveChapterParagraph(chapterParagraph){
-                this.$emit('chapter-paragraph-edited', chapterParagraph)
-            },
-            receiveAdventureParagraph(adventureParagraph){
-                this.$emit('adventure-paragraph-edited', adventureParagraph)
-            },
-            deleteScene(pathObj){
-                this.$emit('delete-scene-editor', pathObj)
-            }
         }
     }
 </script>
