cc6a20a0f2740c87e2e32f2911e67e27881c28fe
agcharis67705 <agcharis67705@th-nuernberg.de>
2019-08-10 20:00:29 +0200

    path rendering in work

--- src/components/tree/Treerenderer.vue
+++ src/components/tree/Treerenderer.vue
@@ -4,6 +4,8 @@
             <g class="tree">
                 <g class="links"></g>
                 <g class="chapters"></g>
+                <g class="paths"></g>
+                <g class="scenes"></g>
             </g>
             <g class="adventure"></g>
         </svg>
@@ -65,9 +67,8 @@
 
                 d3.select('svg g.tree').attr('transform', `translate(0,${2*chapterRadius})`)
 
-                d3.select('svg g.chapters')
-                    .selectAll('circle')
-                    .on('dragover', function () {
+                d3.selectAll('svg g.chapters circle')
+                    .on('dragenter', function () {
                         d3.select(this)
                             .attr('r', 25)
                             .style("fill", "red")
@@ -77,8 +78,76 @@
                             .attr('r', 14)
                             .style("fill", "steelblue")
                     })
+
+                drawScenes(parent, child){
+                    if (child.scenes){
+                        for(i = 0; i < child.scenes.length; i++){
+                            let path = child.scenes[i]
+                            drawPath(child, path, parent, i)
+                        }
+                    }
+                }
+
+                drawPath(child, path, parent, i){
+                    const sourceV = {x: parent.x, y: parent.y}
+                    const targetV = {x: child.x, y: child.y}
+                    const distanceVector = vSub(targetV, sourceV)
+                    const vStep = vScale(distanceVector, 1/3)
+                    const distance = length(distanceVector)
+                    let pathString;
+
+                    if(i == 0) pathString = `M ${sourceX} ${sourceY} L ${targetX} ${TargetY}`
+                    
+                    else{
+                        if (i % 2 == 0) i = -(i-1)
+                        
+                        vC = vScale(vOrthNorm(vStep), i*0.2*distance)
+                        let C1 = sourceV + vStep + vC
+                        let C2 = sourceV + vScale(vStep, 2) + vC
+
+                        pathString = `M ${sourceX} ${sourceY} C ${C1.x} ${C1.y}, ${C2.x} ${C2.y}, ${targetX} ${TargetY}`
+                    }
+
+                    d3.select('svg g.scenes')
+                        .append('path')
+                            .attr('d', pathString)
+
+                    let svg = d3.select('svg g.paths')
+                    for(j = 0; j < path.length; i++){
+
+                    }
+                }
             }
         }
+    }
+
+    vOrthNorm(v){
+        hyp = length(v)
+        return {
+            x: -(v.y/hyp),
+            y: v.x/hyp
+        }
+    }
+
+    vScale(v, s){
+        return {x: v.x*s, y: v.y*s}
+    }
+
+    vAdd(v1, v2){
+        return {x: v1.x+v2.x, y: v1.y+v2.y}
+    }
+
+    vSub(v1, v2){
+        return {x: v1.x-v2.x, y: v1.y-v2.y}
+    }
+
+    distance(p1, p2){
+        vDist = {x: (p1.x-p2.x), y:(p1.y-p2.y)}
+        return length(vDist)
+    }
+
+    length(v){
+        return Math.sqrt(vDist.x**2 + vDist.y**2)
     }
 </script>
 
