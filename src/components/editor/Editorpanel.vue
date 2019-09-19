3b71aa36a888c11630259485cfa6e881b40dac4b
agcharis67705 <agcharis67705@th-nuernberg.de>
2019-09-19 12:53:19 +0200

    editing affects state and is rendered correctly

--- src/components/editor/Editorpanel.vue
+++ src/components/editor/Editorpanel.vue
@@ -6,14 +6,16 @@
 </template>
 
 <script>
+    import * as DataElements from '../../util/DataElements.js'
+
     export default {
         props: {
             storyItem: Object
         },
         data: function() {
             return {
-                isChapter: this.storyItem.type === 'chapter',
-                isScene: this.storyItem.type === 'scene',
+                isChapter: this.storyItem instanceof DataElements.Chapter,
+                isScene: this.storyItem instanceof DataElements.Scene,
                 oldStoryItem: {}
             }
         },
@@ -21,15 +23,15 @@
             type: function(){ return this.storyItem.type }
         },
         methods: {
-            testEventTitle: function (inputEvent) {
-                console.log(inputEvent)
-                let message = `old title: ${this.storyItem.title}\n new title: ${inputEvent}`
-                alert(message)
+            testEventTitle: function (focusOutEvent) {
+                let oldTitle = this.storyItem.title
+                let newTitle = focusOutEvent.srcElement.textContent
+                if(oldTitle !== newTitle) this.storyItem.title = newTitle
             },
-            testEventText: function (inputEvent) {
-                console.log(inputEvent)
-                let message = `old text: ${this.storyItem.text}\n new text: ${inputEvent}`
-                alert(message)
+            testEventText: function (focusOutEvent) {
+                let oldText = this.storyItem.text
+                let newText = focusOutEvent.srcElement.textContent
+                if(oldText !== newText) this.storyItem.text = newText
             }
         }
     }
