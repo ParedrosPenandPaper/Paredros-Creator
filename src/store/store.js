0d4d274a2d19c5c90921f9664facdb2ebf6e11d2
Ismael Agchar <ismael.agchar@siemens-healthineers.com>
2019-11-18 18:47:17 +0100

    fixed modal dixplay bug on updateContent

--- src/store/store.js
+++ src/store/store.js
@@ -111,8 +111,8 @@ export const store = new Vuex.Store({
                 state.modal.type = null
                 state.currentDropTarget.value = null
                 state.currentDropTarget.type = null
+                state.content.current = null
             }
-            state.content.current = null
             
             state.adventureObject = [...state.adventureObject]
         },
@@ -211,6 +211,8 @@ export const store = new Vuex.Store({
                 state.adventureObject[2].item[foundIndexItem].name = content.name
                 state.adventureObject[2].item[foundIndexItem].text = content.text
             }
+
+            state.content.current = null
         }
     }
 })
